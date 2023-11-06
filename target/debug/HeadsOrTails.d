f3daa62ffbf4d12356696820e149dd4aa47d9f0f
rottakore <88859658+rottakore@users.noreply.github.com>
2023-11-05 21:43:57 -0300

    Delete target directory

--- target/debug/HeadsOrTails.d
+++
@@ -1,1 +0,0 @@
-C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HeadsOrTails\target\debug\HeadsOrTails.exe: C:\Users\lu\Documents\Projetos\ e\ Anotações\Rust\HeadsOrTails\src\main.rs
--- target/debug/deps/rand_chacha-903d760bb7f19be4.d
+++
@@ -1,9 +0,0 @@
-C:\Users\lu\Documents\Projetos e Anotações\Rust\HeadsOrTails\target\debug\deps\rand_chacha-903d760bb7f19be4.rmeta: C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\lu\Documents\Projetos e Anotações\Rust\HeadsOrTails\target\debug\deps\librand_chacha-903d760bb7f19be4.rlib: C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\lu\Documents\Projetos e Anotações\Rust\HeadsOrTails\target\debug\deps\rand_chacha-903d760bb7f19be4.d: C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
-C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
-C:\Users\lu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
